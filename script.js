7eda83d2a4abf01fa4808cbbf6fd7a6f6d317e6c
Matthew Cheney <matt1cheney@gmail.com>
2020-02-10 09:03:00 -0500

    add comments and add urls in readme

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 $(function() {
+  // global varaibles
   let forecasturl = "https://api.openweathermap.org/data/2.5/forecast?q=";
-  // {city name},{country code}
   let apiKey = "973464612f77fc58086327531b8be6f9";
   let weatherSearch = "";
   let currentWeatherurl = "https://api.openweathermap.org/data/2.5/weather?q=";
   let buttonCount = 0;
+  // grabbing buttons from local storage
   localStorage.getItem($("derry"));
   $(".fa-search").on("click", function() {
+    // clears the 5 day forcast containers
     $(".day").empty();
     weatherSearch = $(".citySearch").val();
-
+    // create api request for current weather
     let currentWeatherCall =
       currentWeatherurl +
       weatherSearch +
       ",us&mode=json&units=imperial&appid=" +
       apiKey;
+    // create api request for 5 day weather
     let dayWeatherCall =
       forecasturl +
       weatherSearch +
       ",us&mode=json&units=imperial&appid=" +
       apiKey;
     buttonCount++;
+    // create button and give button classes
     $(".cityList").append(
       $("<button>", "<br/>")
-        .addClass(`col-12 btn btn-secondary btn-lg cityButton button${buttonCount}`)
+        .addClass(
+          `col-12 btn btn-secondary btn-lg cityButton button${buttonCount}`
+        )
         .text(weatherSearch)
     );
+    //store button to local storage
     localStorage.setItem($(`.button${buttonCount}`).text(), "string");
-    
-    
+
+    // fire the 5day forecast and current weather forecast
     get5DayForecast();
     currentWeather();
-
+    // function for current weather
     function currentWeather() {
       $.get(currentWeatherCall, function(response) {
-      
+        // show date and icon
         $(".date").text(moment().format("MM/ DD/ YY"));
         $(".date").append(
           $("<img>").attr(
@@ -42,27 +49,28 @@ $(function() {
             `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
           )
         );
+        // show city name
         $(".cityName").text(response.name);
-
+        // print temp, humidity, and windspeed
         $(".temp").text(`temp: ${Math.floor(response.main.temp)} F`);
         $(".humidity").text(`Humidity: ${response.main.humidity} %`);
         $(".windspeed").text(`Windspeed: ${response.wind.speed} MPH`);
       });
     }
-
+    // grab data
     function parseForecastData(forecast) {
-     
       let currentDay = "00";
       let dayCount = 0;
       let hourCount = 43200;
       let hourSelect = moment().add(`${hourCount}`, "hours");
       forecast.forEach(timeBlock => {
-        //variables
+        // variables
         let day = moment.unix(timeBlock.dt).format("DD");
         let hour = moment.unix(timeBlock.dt).format("HH");
         tempVal = Math.floor(timeBlock.main.temp);
         humidVal = timeBlock.main.humidity;
         windSpeed = timeBlock.wind.speed;
+        // if statement for showing 
         if (day != currentDay) {
           currentDay = day;
 
@@ -77,12 +85,14 @@ $(function() {
                 .format("M/D/YY")
             )
           );
+          // creates icons in the 5 day
           $(`.day${dayCount}`).append(
             $("<img>").attr(
               "src",
               `https://openweathermap.org/img/wn/${timeBlock.weather[0].icon}@2x.png`
             )
           );
+          // tempurature and humidity in 5 day containers
           $(`.day${dayCount}`).append($("<li>").text(`Temp: ${tempVal} F`));
           $(`.day${dayCount}`).append($("<li>").text(`Humidity: ${humidVal}%`));
         }
@@ -90,7 +100,6 @@ $(function() {
         forDate = moment
           .unix(timeBlock.dt)
           .format("dddd, MMMM, Do, YYYY h:mm:ss A");
-      
 
         //day 1
 
@@ -137,7 +146,6 @@ $(function() {
     }
     function get5DayForecast() {
       $.get(dayWeatherCall, function(response) {
-    
         let forecastArr = response.list;
         parseForecastData(forecastArr);
         // weatherCall.forEach(uvVal => {
@@ -145,12 +153,13 @@ $(function() {
         // });
       });
     }
+    // event listener for button 
     $(`.button${buttonCount}`).on("click", function() {
-     $(".day").empty();
-
-       currentWeather();
-       get5DayForecast();
+      // when clicked empty 5 day containers
+      $(".day").empty();
+      // fire with the button value 
+      currentWeather();
+      get5DayForecast();
     });
   });
-
 });
